a74dde91871558fe89a07986d05265927c10ec8a
Kat <Kat@RayForComW1015>
2020-09-27 03:43:33 -0400

    #19 Preferences for Board Game Layout

---
+++ course-project-cheakmate/public/settings.js
@@ -0,0 +1,26 @@
+//@Author: Kat
+function center(){
+  document.getElementById('board').style.textAlign = "center";
+  document.getElementById('board').style.margin = "auto";
+}
+
+function toLeft(){
+  document.getElementById('board').style.float = "left";
+}
+
+function toRight(){
+  document.getElementById('board').style.float = "right";
+}
+
+function chessBg(){
+  document.body.style.backgroundImage = "url('images/Default.jpg')";
+}
+
+function toBg1(){
+  document.body.style.backgroundImage = "url('images/Lake.jpg')";
+
+}
+
+function toBg2(){
+  document.body.style.backgroundImage = "url('images/Galaxy.jpg')";
+}
