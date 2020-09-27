d6f487deea31a6c2ea79379c505103ff2793a514
Kat <Kat@RayForComW1015>
2020-09-27 19:02:24 -0400

    Manual Webserver created from CSE312 (temporary use, will be improved)

---
+++ course-project-cheakmate/src/StatusUpdate.java
@@ -0,0 +1,129 @@
+import java.util.Date;
+
+import javax.net.ssl.SSLEngineResult.Status;
+
+public class StatusUpdate {
+
+	private String STATUS; 
+	private String USERNAME;
+	private String TIME;
+	
+	/**
+	 * Just a post from anonymous 
+	 * @param status
+	 */
+	public StatusUpdate(String status) {
+		STATUS = status; 
+		USERNAME = "anonymous";
+		TIME = "" + new Date();
+	}
+	
+	/**
+	 * A new post from a user 
+	 * @param status
+	 * @param username
+	 */
+	public StatusUpdate(String status, String username) {
+		status = status.replace("&", "&amp");
+		status = status.replace("<", "&lt");
+		status = status.replace(">", "&gt");
+		STATUS = status; 
+		username = username.replace("&", "&amp");
+		username = username.replace("<", "&lt");
+		username = username.replace(">", "&gt");
+		USERNAME = username;
+		TIME = "" + new Date();
+	}
+	
+	/**
+	 * An existing post from a user
+	 * @param status
+	 * @param username
+	 * @param time
+	 */
+	public StatusUpdate(String status, String username, String time) {
+		status = status.replace("&", "&amp");
+		status = status.replace("<", "&lt");
+		status = status.replace(">", "&gt");
+		STATUS = status; 
+		username = username.replace("&", "&amp");
+		username = username.replace("<", "&lt");
+		username = username.replace(">", "&gt");
+		USERNAME = username;
+		TIME = time;
+	}
+	
+	/**
+	 * Generates the post as text
+	 * @return {username} said: {status} @ {time}
+	 */
+	public String getPost() {
+		return USERNAME + " said: " + STATUS + " @" + TIME; 
+	}
+	
+	/**
+	 * Generates the post as text with the oting feature
+	 * TODO Terence if you can fix this please
+	 * @return {username} said: {status} @ {time} as well as the voting bar
+	 */
+	public String getFullPost() {
+		return  "<br />" +
+				"<li class=\"vote\">\r\n" + 
+				USERNAME + " said:" +
+				"<br />" +
+				STATUS +
+				"<br />" +
+				" @" + TIME +
+				"<hr />" +
+				"      <button class=\"upclick\" onclick=\"upvote(1)\">up Vote</button>\r\n" + 
+				"      <span id=\"currVotes {{ votes }} \"></span>\r\n" + 
+				"      <button class=\"downclick\" onclick=\"downvote(1)\">down Vote</button>\r\n" + 
+				"    </li>" +
+				"<br />";
+	}
+	
+	/**
+	 * get the post as a CSV
+	 * @return {username},{status},{time}
+	 */
+	public String getCSV() {
+		return STATUS + "," + USERNAME + "," + TIME; 
+	}
+
+	/**
+	 * Get the status only
+	 * @return
+	 */
+	public String getSTATUS() {
+		return STATUS;
+	}
+
+	/**
+	 * returns the username only
+	 * @return
+	 */
+	public String getUSERNAME() {
+		return USERNAME;
+	}
+
+	/**
+	 * returns the time the post was made
+	 * @return
+	 */
+	public String getTIME() {
+		return TIME;
+	}
+	
+	/**
+	 * Compares to post to make sure they aren't the same
+	 * @param post the other post you're comparing this one to 
+	 * @return true if they are the same post
+	 */
+	public boolean compare(StatusUpdate update) {
+		boolean notSame = false;
+		if(update.getPost().equals(getPost())) notSame = true;
+		return notSame;
+	}
+	
+	
+}
