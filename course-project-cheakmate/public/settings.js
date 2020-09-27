16207b7f299207f4f6b367a876cc947f4c273129
Alex Lin <Alex Lin@LAPTOP-QPCDN3BR>
2020-09-27 17:26:09 -0400

    Edited the function center() and got board to center

---
+++ course-project-cheakmate/public/settings.js
@@ -0,0 +1,25 @@
+//@Author: Kat
+function center(){
+  document.getElementById('board').style.float = "none";
+}
+
+function toLeft(){
+  document.getElementById('board').style.float = "left";
+}
+
+function toRight(){
+  document.getElementById('board').style.float = "right";
+}
+
+function chessBg(){
+  document.body.style.backgroundImage = "url('images/Default.jpg')";
+}
+
+function toBg1(){
+  document.body.style.backgroundImage = "url('images/Lake.jpg')";
+
+}
+
+function toBg2(){
+  document.body.style.backgroundImage = "url('images/Galaxy.jpg')";
+}
