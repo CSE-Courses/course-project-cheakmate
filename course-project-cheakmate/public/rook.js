639f34e19ad07579c58453d1ee044bf8e24d8648
kw934 <kw934@LAPTOP-1967NRGS>
2020-10-18 01:02:31 -0400

    #40 Highlights Rook's moves
    Currently when a rook is clicked on, it will show anywhere the rook can
    move, if its an empty space, the border of the tile is highlighted blue,
    if it's an enemy piece it will be red. The highlighted pieces do not
    account for collision and can be highlighted even if its a spot behind a
    piece.

--- course-project-cheakmate/public/rook.js
+++ course-project-cheakmate/public/rook.js
@@ -1,43 +1,44 @@
-//@Author: Alam
-//@Editor : Alex & Kat & Kevin & Richard
-var board; //2-D array keeping image sources for board
+/*@Author: Alam
+@Editor : Alex & Kat & Kevin & Richard 
+*/
+var board; /*2-D array keeping image sources for board*/
 
 function initialize(){
-  //Create a 10 x 10 board
+  /*Create a 10 x 10 board*/
   board = new Array(10);
   for(var i = 0; i < board.length; i++){
     board[i] = new Array(10);
   }
 
-  //Set up a 10 x 10 board
+  /*Set up a 10 x 10 board*/
 
-  //Set up Rooks
+  /*Set up Rooks*/
   board[0][1] = "images/sprites/dob.png";
   board[0][8] = "images/sprites/lob.png";
   board[9][1] = "images/sprites/low.png";
   board[9][8] = "images/sprites/dow.png";
 
-  //Set up Knights
+  /*Set up Knights*/
   board[0][2] = "images/sprites/lkb.png";
   board[0][7] = "images/sprites/dkb.png";
   board[9][2] = "images/sprites/dkw.png";
   board[9][7] = "images/sprites/lkw.png";
 
-  //Set up Bishops
+  /*Set up Bishops*/
   board[0][3] = "images/sprites/dhb.png";
   board[0][6] = "images/sprites/lhb.png";
   board[9][3] = "images/sprites/lhw.png";
   board[9][6] = "images/sprites/dhw.png";
 
-  //Set up Kings
+  /*Set up Kings*/
   board[0][5] = "images/sprites/dgb.png";
   board[9][5] = "images/sprites/lgw.png";
 
-  //Set up Queens
+  /*Set up Queens*/
   board[0][4] = "images/sprites/lqb.png";
   board[9][4] = "images/sprites/dqw.png";
 
-  //Set up Empty Tiles
+  /*Set up Empty Tiles*/
   for(var i = 0; i < board.length; i++){
     for(var j = 0; j < board[i].length; j++){
       if(i % 2 == 0 && j == 0 && board[i][j] == null){
@@ -69,7 +70,7 @@ function initialize(){
         }
       }
 
-      //Set up Pawns
+      /*Set up Pawns*/
       if (i == 1) {
           if(j % 2 == 0 && board[i][j] == null){
             board[i][j] = "images/sprites/dpb.png";
@@ -89,11 +90,12 @@ function initialize(){
     }
   }
 
-  //Display board
+
+  /*Display board*/
   populate();
 }
 
-//Reads the 2-D array and changes the image source accoring to its image id
+/*Reads the 2-D array and changes the image source accoring to its image id */
 function populate(){
   var num;
   for(var i = 0; i < board.length; i++){
@@ -104,23 +106,27 @@ function populate(){
   }
 }
 
-//Exits the game and returns to main menu
+/*Exits the game and returns to main menu */
 function exit(){
   if (confirm("Are you sure you want to leave the game?")) {
     window.location.href = "index.html";
   }
 }
 
-//Changes border color of image
-//Happens when the image is selected
-//Happens when possible moves are shown
+/*
+Changes border color of image
+Happens when the image is selected
+Happens when possible moves are shown
+*/
 function changeBorderColor(imageId, color){
   document.getElementById(imageId).style.border = "3px solid " + color;
 }
 
-//Resets border color of all images
-//Happens when the image selected is changed
-//Happens when turn is over
+/*
+Resets border color of all images
+Happens when the image selected is changed
+Happens when turn is over
+*/
 function resetBorderColor(){
   var num;
   for(var i = 0; i < board.length; i++){
@@ -131,11 +137,13 @@ function resetBorderColor(){
   }
 }
 
-//Each image onclick calls for the function to run game
-//This works by
-//Checking the type of chess piece
-//Finding the possible moves of the chess piece
-//Moving the chess piece
+/*
+Each image onclick calls for the function to run game
+This works by
+Checking the type of chess piece
+Finding the possible moves of the chess piece
+Moving the chess piece\
+*/
 function runGame(imageId){
   resetBorderColor();
   var r = parseInt(imageId.substring(0,1));
@@ -149,30 +157,34 @@ function runGame(imageId){
     showPossiblePawnMoves(r, c);
   }
 
-  // TODO:
-  //Checks if it is a Rook here
-    //If it is, show possible moves
-      //Make a move
+  /* TODO:
+  Checks if it is a Rook here
+    If it is, show possible moves
+      Make a move */
+  
+  if(isRook(r, c)){
+	    showPossibleRookMoves(r, c);
+  }
+  
+  /* TODO:
+  Checks if it is a Knights here
+    If it is, show possible moves
+      Make a move */
 
-  // TODO:
-  //Checks if it is a Knights here
-    //If it is, show possible moves
-      //Make a move
+  /* TODO:
+  Checks if it is a Bishops here
+    If it is, show possible moves
+      Make a move */
 
-  // TODO:
-  //Checks if it is a Bishops here
-    //If it is, show possible moves
-      //Make a move
+  /* TODO:
+  Checks if it is a King here
+    If it is, show possible moves
+      Make a move */
 
-  // TODO:
-  //Checks if it is a King here
-    //If it is, show possible moves
-      //Make a move
-
-  // TODO:
-  //Checks if it is a Queen here
-    //If it is, show possible moves
-      //Make a move
+  /* TODO:
+  Checks if it is a Queen here
+    If it is, show possible moves
+      Make a move */
 }
 
 function isEmptyTile(r, c){
@@ -193,12 +205,23 @@ function isPawn(r, c){
   }
 }
 
-//Conditions are made in order and according to the following rules
-//Rule 1 : Pawns can only move forward one square at a time
-//Rule 1.5 : Pawns can not move backwards
-//Rule 2 : Pawns can move forward two squares for their very first move
-//Rule 2.5 : There must be no chess pieces in between the two squares
-//Rule 3 : Pawns can only capture one square diagonally in front of them
+function isRook(r, c){
+	if(board[r][c].includes("ob") || board[r][c].includes("ow")){
+	    alert("I am " + board[r][c]);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/*Conditions are made in order and according to the following rules
+Rule 1 : Pawns can only move forward one square at a time
+Rule 1.5 : Pawns can not move backwards
+Rule 2 : Pawns can move forward two squares for their very first move
+Rule 2.5 : There must be no chess pieces in between the two squares
+Rule 3 : Pawns can only capture one square diagonally in front of them
+*/
 function showPossiblePawnMoves(r, c){
   var num;
 
@@ -256,3 +279,86 @@ function showPossiblePawnMoves(r, c){
     }
   }
 }
+/*By Kevin*/
+function showPossibleRookMoves(r, c){
+	var num;
+	/*Moving Rook upwards*/
+	for(var up = r; up >= 0; up--){
+		/*alert("My position is " + up + ", " + c);*/
+		if(up >= 0 && (board[up][c].includes("l.png") || board[up][c].includes("d.png"))){
+			num = "" + up + c;
+			changeBorderColor(num, "#006699");
+			
+		}
+		/*Only while you're white, pieces are the black pieces red*/
+		if(board[up][c].includes("b.png") && board[r][c].includes("ow")){
+			num = "" + up + c;
+			changeBorderColor(num, "#ff0000");
+		}
+		/*Only while you're black, pieces are the white pieces red*/
+		if(board[up][c].includes("w.png") && board[r][c].includes("ob")){
+			num = "" + up + c;
+			changeBorderColor(num, "#ff0000");
+		}
+	}
+	/*Moving Rook downwards*/
+	for(var down = r; down <= 9; down++){
+		/*alert("My position is " + down + ", " + c);*/
+		if(down <= 9 && (board[down][c].includes("l.png") || board[down][c].includes("d.png"))){
+			num = "" + down + c;
+			changeBorderColor(num, "#006699");
+			
+		}
+		/*Only while you're white, pieces are the black pieces red*/
+		if(board[down][c].includes("b.png") && board[r][c].includes("ow")){
+			num = "" + down + c;
+			changeBorderColor(num, "#ff0000");
+
+		}
+		/*Only while you're black, pieces are the white pieces red*/
+		if(board[down][c].includes("w.png") && board[r][c].includes("ob")){
+			num = "" + down + c;
+			changeBorderColor(num, "#ff0000");
+
+		}
+	}
+	/*Moving Rook left*/
+	for(var left = c; left >= 0; left--){
+		/*alert("My position is " + r + ", " + left);*/
+		if(left >= 0 && (board[r][left].includes("l.png") || board[r][left].includes("d.png"))){
+			num = "" + r + left;
+			changeBorderColor(num, "#006699");
+		}
+		/*Only while you're white, pieces are the black pieces red*/
+		if(board[r][left].includes("b.png") && board[r][c].includes("ow")){
+			num = "" + r + left;
+			changeBorderColor(num, "#ff0000");
+		}
+		/*Only while you're black, pieces are the white pieces red*/
+		if(board[r][left].includes("w.png") && board[r][c].includes("ob")){
+			num = "" + r + left;
+			changeBorderColor(num, "#ff0000");
+		}
+	}
+	/*Moving Rook right*/
+	for(var right = c; right <= 9; right++){
+		/*alert("My position is " + r + ", " + right);*/
+		if(right <= 9 && (board[r][right].includes("l.png") || board[r][right].includes("d.png"))){
+			num = "" + r + right;
+			changeBorderColor(num, "#006699");
+		}
+		/*Only while you're white, pieces are the black pieces red*/
+		if(board[r][right].includes("b.png") && board[r][c].includes("ow")){
+			num = "" + r + right;
+			changeBorderColor(num, "#ff0000");
+		}
+		/*Only while you're black, pieces are the white pieces red*/
+		if(board[r][right].includes("w.png") && board[r][c].includes("ob")){
+			num = "" + r + right;
+			changeBorderColor(num, "#ff0000");
+		}
+	}
+
+	
+}
+
